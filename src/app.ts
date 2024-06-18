57ab3872fee13e5dd7ca8aa021891f269fa29cf9
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-06-18 15:06:38 +0300

    change main file

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,10 @@ import authRoute from './routes/auth';
 import taskRoute from './routes/task';
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/auth', authRoute);
 app.use('/tasks', taskRoute);
-
-app.listen(port, () => {
-	console.log(`Server is running at http://localhost:${port}`);
-});
+export default app;
