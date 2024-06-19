65df22aa6ffc07ca276493ccd1f30d9082ea6ab0
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-06-19 19:43:19 +0300

    add task routes

--- src/routes/task.ts
+++ src/routes/task.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import taskController from '../controllers/task';
+import taskController from '../controllers/task.controller';
 
 const router: Router = Router();
 
 router.route('/').post(taskController.create);
-router.route('/').get(taskController.getAll);
-router.route('/:taskId').get(taskController.getOne);
-router.route('/:taskId').put(taskController.update);
-router.route('/:taskId').delete(taskController.delete);
+// router.route('/').get(taskController.getAll);
+// router.route('/:taskId').get(taskController.getOne);
+// router.route('/:taskId').put(taskController.update);
+// router.route('/:taskId').delete(taskController.delete);
 
 export default router;
