ad97b7b11ebb7fb08de5534e5ecb2cc1afb195ad
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-06-19 20:14:22 +0300

    fix

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -16,13 +16,13 @@ const login = catchAsync(async ({ body: { email, password } }, res) => {
     if (!email) throw new ApiError(httpStatus.BAD_REQUEST, 'Missing email');
     if (!password) throw new ApiError(httpStatus.BAD_REQUEST, 'Missing password');
 
-    const data = await authService.register(email, password);
+    const data = await authService.login(email, password);
     res.cookie('refreshToken', data.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
     res.json(data);
 });
 
 const logout = catchAsync(async (req, res) => {
-    const refreshToken = req.cookies;
+    const refreshToken = req.cookies.refreshToken;
     await authService.logout(refreshToken);
     res.clearCookie('refreshToken');
     res.json({ status: 'OK' });
