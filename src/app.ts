3750c65e5c29b1d1b62d1d83393579b85b1239fa
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-06-19 19:39:09 +0300

    add uses

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv'; // eslint-disable-line
+import corsMiddleware from './middlewares/cors';
+import cookieParser from 'cookie-parser';
 
 import authRoute from './routes/auth';
 import taskRoute from './routes/task';
@@ -8,6 +10,8 @@ const app: Express = express();
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
+app.use(corsMiddleware);
+app.use(cookieParser());
 
 app.use('/auth', authRoute);
 app.use('/tasks', taskRoute);
