2db67816aa1371c60a2648558b0f1619654e5198
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-06-19 20:14:15 +0300

    add user-dto usage

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,41 +2,49 @@ import ApiError from '../utils/ApiError';
 import httpStatus from 'http-status';
 import userService from './user.service';
 import jwtUtil from '../utils/jwt';
+import UserDto from '../dtos/user-dto';
+import { isPasswordMatch } from '../utils/encryption';
 import { User } from '@prisma/client';
 
 type data = {
     accessToken: string;
     refreshToken: string;
-    user: User;
+    user: UserDto;
 };
 const register = async (email: string, password: string): Promise<data> => {
     if (await userService.getUserByEmail(email)) {
         throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
     }
     const user = await userService.createUser(email, password);
-    const { accessToken, refreshToken } = jwtUtil.generate({ ...user });
+    const userDto = new UserDto(user);
+    const { accessToken, refreshToken } = jwtUtil.generate({ ...userDto });
     await jwtUtil.saveToken(user.id, refreshToken);
 
     return {
         accessToken: accessToken,
         refreshToken: refreshToken,
-        user: user,
+        user: userDto,
     };
 };
 
 const login = async (email: string, password: string): Promise<data> => {
-    if (await userService.getUserByEmail(email)) {
-        throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
+    if (!(await userService.getUserByEmail(email))) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
     }
-    const user = await userService.createUser(email, password);
-    const { accessToken, refreshToken } = jwtUtil.generate({ ...user });
+    const user = (await userService.getUserByEmail(email)) as User;
+    // if (!user || !user.validatePassword(password)) throw new AppErrorInvalid("login or password");
+    if (!isPasswordMatch(password, user.password)){
+        throw new ApiError(httpStatus.BAD_REQUEST, 'Wrong password');
+    }
+    const userDto = new UserDto(user);
+    const { accessToken, refreshToken } = jwtUtil.generate({ ...userDto });
 
     await jwtUtil.saveToken(user.id, refreshToken);
 
     return {
         accessToken: accessToken,
         refreshToken: refreshToken,
-        user: user,
+        user: userDto,
     };
 };
 
