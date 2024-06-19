ace6d10fddd3c2c5874d3034aeee2ea250903643
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-06-19 23:43:31 +0300

    add task CRUD

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -2,6 +2,7 @@ import httpStatus from 'http-status';
 import catchAsync from '../utils/catchAsync';
 import ApiError from '../utils/ApiError';
 import taskService from '../services/task.service';
+import TaskDto from '../dtos/task-dto';
 
 const create = catchAsync(async (req, res) => {
     const { title, description } = req.body;
@@ -11,9 +12,71 @@ const create = catchAsync(async (req, res) => {
 
     const task = await taskService.createTask(title, description, user.id);
 
-    res.json(task);
+    const taskDto = new TaskDto(task);
+
+    res.json(taskDto);
+});
+
+const getAll = catchAsync(async (req, res) => {
+    const user = req.user;
+    const tasks = await taskService.getAllUserTasks(user.id);
+
+    const taskDtos = tasks.map(task => new TaskDto(task));
+
+    res.json(taskDtos);
+});
+
+const getOne = catchAsync(async (req, res) => {
+    const { taskId } = req.params;
+    if (!taskId) {
+        throw new ApiError(httpStatus.BAD_REQUEST, 'Missing taskId');
+    }
+
+    const task = await taskService.getTaskById(Number(taskId));
+    if (!task) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'Task not found');
+    }
+
+    const taskDto = new TaskDto(task);
+
+    res.json(taskDto);
+});
+
+const update = catchAsync(async (req, res) => {
+    const { taskId } = req.params;
+    if (!taskId) {
+        throw new ApiError(httpStatus.BAD_REQUEST, 'Missing taskId');
+    }
+
+    const updateBody = req.body;
+    if (!(updateBody.title || updateBody.description || updateBody.status)) {
+        throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid body');
+    }
+    if (updateBody.status) {
+        updateBody.status = Boolean(updateBody.status);
+    }
+
+    const task = await taskService.updateTaskById(Number(taskId), updateBody);
+
+    const taskDto = new TaskDto(task);
+
+    res.json(taskDto);
+});
+
+const deleteTask = catchAsync(async (req, res) => {
+    const { taskId } = req.params;
+    if (!taskId) {
+        throw new ApiError(httpStatus.BAD_REQUEST, 'Missing taskId');
+    }
+    await taskService.deleteTaskById(Number(taskId));
+
+    res.json({ status: 'OK' });
 });
 
 export default {
     create,
+    getAll,
+    getOne,
+    update,
+    deleteTask,
 };
--- src/routes/task.ts
+++ src/routes/task.ts
@@ -5,9 +5,9 @@ import verifyToken from '../middlewares/verify-token';
 const router: Router = Router();
 
 router.route('/').post(verifyToken.auth, taskController.create);
-// router.route('/').get(taskController.getAll);
-// router.route('/:taskId').get(taskController.getOne);
-// router.route('/:taskId').put(taskController.update);
-// router.route('/:taskId').delete(taskController.delete);
+router.route('/').get(verifyToken.auth, taskController.getAll);
+router.route('/:taskId').get(verifyToken.auth, taskController.getOne);
+router.route('/:taskId').put(verifyToken.auth, taskController.update);
+router.route('/:taskId').delete(verifyToken.auth, taskController.deleteTask);
 
 export default router;
