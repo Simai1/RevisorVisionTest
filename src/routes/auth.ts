37b70e6a7d100e3afc0bdaf07b2e2b7da3ef0edc
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-06-19 19:41:06 +0300

    add logout

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,5 +5,5 @@ const router: Router = Router();
 
 router.route('/register').post(authController.register);
 router.route('/login').post(authController.login);
-
+router.route('/logout').post(authController.logout);
 export default router;
