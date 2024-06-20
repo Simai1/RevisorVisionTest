9498503f910ea658760bf8bf13740da2e8d1570a
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-06-20 09:45:32 +0300

    eslint

--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,26 @@ import dotenv from 'dotenv'; // eslint-disable-line
 
 let server: Server;
 prisma.$connect().then(() => {
-	console.log('Connected to SQL Database');
-	server = app.listen(process.env.PORT, () => {
-		console.log(`Listening to port ${process.env.PORT}`);
-	});
+    console.log('Connected to SQL Database');
+    server = app.listen(process.env.PORT, () => {
+        console.log(`Listening to port ${process.env.PORT}`);
+    });
 });
 
 const exitHandler = () => {
-	if (server) {
-		server.close(() => {
-			console.log('Server closed');
-			process.exit(1);
-		});
-	} else {
-		process.exit(1);
-	}
+    if (server) {
+        server.close(() => {
+            console.log('Server closed');
+            process.exit(1);
+        });
+    } else {
+        process.exit(1);
+    }
 };
 
 const unexpectedErrorHandler = (error: unknown) => {
-	console.log(error);
-	exitHandler();
+    console.log(error);
+    exitHandler();
 };
 
 process.on('uncaughtException', unexpectedErrorHandler);
